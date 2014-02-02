25c8d448401aa410edecc614fba8ae48a71efbfb
Irene <irene@aurametrix.com>
2014-02-02 10:17:54 -0800

    parsing

--- REGEX/regex.lua
+++ REGEX/regex.lua
@@ -4,5 +4,38 @@ string.match("abcdefg", 'c...') -- find and return the matched text
 
 --[[
 +  - one or more times, [] - classes, %d - digit
+() grouping, * zero or more, ? one or more, | or
 --]]
 
+--[[
+Parsing into syntax tree from left to right 
+]]--
+local function parse(input)
+	local s  = stack.new_stack()
+	--node list is built such that the node appears in reverse polish notation.
+	local node_list = {}
+
+	--shift each iteration
+	for i=1,string.len(input),1 do
+		local c = string.sub(input, i, i)
+		local lexval = {lexer(c)}
+		stack.push(s, lexval)
+		while true do
+			local node = try_reduce(s)
+			if node == nil then
+				break
+			end
+			table.insert(node_list, #node_list+1, node)
+		end
+	end
+	stack.push(s, {token.END})
+	try_reduce(s)
+
+	if stack.peek(s)[1] ~= token.S then
+		return nil
+	end
+	--build the tree
+	local tree = build_tree(node_list)
+	--print(dump_tree(tree))
+	return tree
+end
