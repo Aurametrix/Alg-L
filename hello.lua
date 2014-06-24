34554af1d683b0753aa61643127ebf6c475d5946
Irene <irene@aurametrix.com>
2014-06-23 20:00:00 -0700

    wait

--- hello.lua
+++ hello.lua
@@ -1,1 +1,6 @@
 io.write("Hello world, it's ",_VERSION,"!\n")
+
+print("hello")
+wait(1) 			-- wait here for oen second, to ease the tension)
+print("world")
+print("finished after " .. now() .. " seconds")
