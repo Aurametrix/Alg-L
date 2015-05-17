4a67f8fba740a0c0cbdc1ce897b84b750819fb1c
Aurametrix <irene@aurametrix.com>
2015-05-17 10:15:47 -0700

    faster with LuaSocket
    LuaSocket is a Lua extension library that is composed by two parts: a C core that provides support for the TCP and UDP transport layers, and a set of Lua modules that add support for functionality commonly needed by applications that deal with the Internet.

--- SORT/sleep.lua
+++ SORT/sleep.lua
@@ -1,6 +1,8 @@
+socket = require 'socket'
+ 
 function sleeprint(n)
-  local t0 = os.time()
-  while os.time() - t0 <= n do
+  local t0 = socket.gettime()
+  while (socket.gettime() - t0)*100 <= n do
     coroutine.yield(false)
   end
   print(n)
@@ -8,7 +10,7 @@ function sleeprint(n)
 end
  
 coroutines = {}
-for i=1, #arg do
+for i=1, #arg do 
   wrapped = coroutine.wrap(sleeprint)
   table.insert(coroutines, wrapped)
   wrapped(tonumber(arg[i]))
@@ -17,7 +19,7 @@ end
 done = false
 while not done do
   done = true
-  for i=#coroutines,1,-1 do
+  for i=#coroutines,1,-1 do 
     if coroutines[i]() then
       table.remove(coroutines, i)
     else
