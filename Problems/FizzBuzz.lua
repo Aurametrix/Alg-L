fc6a3ee7f695eb670fc4bd4a86a2e7053b5458ff
Aurametrix <irene@aurametrix.com>
2015-10-22 22:00:53 -0700

    if/else solution

---
+++ Problems/FizzBuzz.lua
@@ -0,0 +1,14 @@
+Great! 
+
+On 10/29 at 8am I will be on Skype: Aurametrix
+I will let you know if more details come up.
+
+Kort, Bernhard and Johanna will be calling from Germany
+Kort mentioned that he was still investigating the long-term stability of the silver coating. It's probably possibly to modify the coating for different analytes, you could ask him about it. 
+
+The main purpose of the call is to get to know each other better, for future collaborations. 
+
+Phil Young might be joining us from Pine Grove, he worked on nanocomposites for chemical detection and optical systems at NASA.
+
+Best,
+Irene
